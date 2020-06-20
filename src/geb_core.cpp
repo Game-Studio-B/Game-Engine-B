188b1f351bc762ad14b25588c36123faecea2976
Johan <johan.boscher@gmail.com>
2020-06-20 19:19:11 +0200

    Setting Up basic Project workspace

---
+++ src/geb_core.cpp
@@ -0,0 +1,5 @@
+//
+// Created by johan on 6/20/2020.
+//
+
+#include "headers/geb_core.h"
---
+++ src/headers/geb_core.h
@@ -0,0 +1,14 @@
+//
+// Created by johan on 6/20/2020.
+//
+
+#ifndef GAME_ENGINE_B_GEB_CORE_H
+#define GAME_ENGINE_B_GEB_CORE_H
+
+
+class geb_core {
+
+};
+
+
+#endif //GAME_ENGINE_B_GEB_CORE_H
